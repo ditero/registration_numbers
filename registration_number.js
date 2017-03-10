95e274e39bba3828eeefc4ae430bc280c765f172
ditero <anelesigenu@gmail.com>
2017-03-10 11:02:47 +0200

    Stage 3 Development Phase

--- registration_number.js
+++ registration_number.js
@@ -1,15 +1,25 @@
 var InputTxt = document.getElementById('myText');
 var eventBtn = document.getElementById('myBtn');
 var listReg = document.getElementById('listReg')
+var errorMsg = document.getElementById('errorMsg');
 
 var addReg = function addNum(){
   var li = document.createElement('P');
   var mDiv = document.createElement("div");
   mDiv.className = 'container';
-  mDiv.appendChild(li);
-  li.textContent = InputTxt.value;
-  listReg.appendChild(mDiv);
-  console.log(InputTxt.value);
+
+  if (InputTxt.value.length != 0) {
+
+    mDiv.appendChild(li);
+    li.textContent = InputTxt.value;
+    listReg.appendChild(mDiv);
+
+  }
+  else {
+    errorMsg.innerHTML = "Please enter registration number.";
+  }
+
+
 };
 
 eventBtn.addEventListener('click',addReg);
